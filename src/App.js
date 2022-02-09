dfdf9bf8c7451e670d2bf3e876b270506479f772
Polyanasousas <souzaspolyana@gmail.com>
2022-02-09 16:30:22 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>Edit src/App.js and save to reload.</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={ Home } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
