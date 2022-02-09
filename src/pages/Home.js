ceec0e5d53dee337bb53ed8f66b77baa55d37bcf
Matheus Januário <matheusjcx@gmail.com>
2022-02-09 17:43:30 -0300

    requisito 4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { getCategories } from '../services/api'
 
 export default class Home extends React.Component {
+  state = {
+    allCategories: [],
+  }
+
+  componentDidMount() {
+    this.getAllCategories();
+  }
+  getAllCategories = async () => {
+    const allCategories = await getCategories()
+    this.setState({ allCategories });
+  } 
+
   render() {
+    const { allCategories } = this.state;
+    console.log(allCategories);
     return (
       <div>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <ul>
+          {
+            allCategories.map(({ name, id }) => (
+              <li>
+                <label key={ id } data-testid='category' htmlFor={ id }>
+                  <input type='radio'></input>{ name }
+                </label>
+              </li>
+            )) 
+          }
+        </ul>
       </div>
     );
   }
