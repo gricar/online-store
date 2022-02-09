0a99ef7fb3ebb2dcae8de11f8c373461bbaa229b
Angélica Silva <angelica.silva9323@gmail.com>
2022-02-09 17:24:39 -0300

    Corrigidas rotas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import CartIcon from '../components/CartIcon';
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
+        <div id="cart">
+          <CartIcon />
+        </div>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
