31942cecf0350ceb35ba591d56610d5a6a47faee
Luciano <oliveiraluck78@gmail.com>
2022-02-09 15:57:25 -0300

    requisito 1 completo

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
 export async function getCategories() {
-  // Implemente aqui
+  const categories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categoriesJson = await categories.json();
+  return categoriesJson;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (!categoryId && !query) {
+    throw new Error('Insira pelo menos um elemento');
+  }
+  const fetchQueryCategory = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const queryCategoryJson = await fetchQueryCategory.json();
+  return (queryCategoryJson);
 }
