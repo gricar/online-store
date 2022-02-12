0f0cccaae374f2d8aafb96e4a724a5c47ed0dd37
Angélica Silva <angelica.silva9323@gmail.com>
2022-02-11 21:02:09 -0300

    Exibe em tela a quantidade no carrinho

--- src/components/CartIcon.js
+++ src/components/CartIcon.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { ReactComponent as ShoppingCart } from '../images/ShoppingCart.svg';
 
 export default class CartIcon extends Component {
   render() {
+    const { quantity, empty } = this.props;
     return (
       <Link to="/cart" data-testid="shopping-cart-button">
         <ShoppingCart width="50" alt="Shopping Cart" />
+        {empty
+          ? (<p data-testid="shopping-cart-size">0</p>)
+          : (
+            <p data-testid="shopping-cart-size">
+              {
+                quantity
+              }
+
+            </p>)}
       </Link>
     );
   }
 }
+
+CartIcon.propTypes = {
+  quantity: PropTypes.number.isRequired,
+  empty: PropTypes.bool.isRequired,
+}.isRequired;
