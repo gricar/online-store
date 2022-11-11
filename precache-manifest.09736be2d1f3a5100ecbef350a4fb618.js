edf91ccfaf49658974c27c14f2bf0bb3d75a6984
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-11-11 15:17:39 -0300

    Updates

---
+++ precache-manifest.09736be2d1f3a5100ecbef350a4fb618.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ac198863ebbbbbf4009910f2e96026",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "a3692700124b8f48e848",
+    "url": "/online-store/static/css/main.302947f6.chunk.css"
+  },
+  {
+    "revision": "4d047653f48da3290880",
+    "url": "/online-store/static/js/2.2249ad5a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/online-store/static/js/2.2249ad5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3692700124b8f48e848",
+    "url": "/online-store/static/js/main.1c74381a.chunk.js"
+  },
+  {
+    "revision": "22496ea918f96246b60e",
+    "url": "/online-store/static/js/runtime-main.d46b2840.js"
+  },
+  {
+    "revision": "2dba54281a97c1775fdfbf3ec96c13b4",
+    "url": "/online-store/static/media/ShoppingCart.2dba5428.svg"
+  }
+]);
